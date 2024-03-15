f36fd84dd34788b232cc4656e1805ed1aeb52dd3
michalszura <115370163+Malamity@users.noreply.github.com>
2024-03-15 18:31:18 +0100

    Update sortingAlgorithms.cs

--- algorithmEfficiency/algorithmEfficiency/sortingAlgorithms.cs
+++ algorithmEfficiency/algorithmEfficiency/sortingAlgorithms.cs
@@ -4,39 +4,116 @@ using BenchmarkDotNet.Running;
 
 public class SortingAlgorithms
 {
-    // Bubble Sort
-    [Benchmark]
-    public void BubbleSort()
-    {
-        // Your implementation of Bubble Sort algorithm here
-    }
-
-    // Selection Sort
-    [Benchmark]
-    public void SelectionSort()
-    {
-        // Your implementation of Selection Sort algorithm here
-    }
-
+    
     // Insertion Sort
     [Benchmark]
-    public void InsertionSort()
+    public void InsertionSort(int[] array)
     {
-        // Your implementation of Insertion Sort algorithm here
+        int n = array.Length;
+        for (int i = 1; i < n; ++i)
+        {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key)
+            {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
     }
 
     // Merge Sort
     [Benchmark]
-    public void MergeSort()
+    public void MergeSort(int[] arr, int l, int m, int r)
     {
-        // Your implementation of Merge Sort algorithm here
-    }
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
 
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+
+            for (i = 0; i < n1; ++i)
+                L[i] = arr[l + i];
+            for (j = 0; j < n2; ++j)
+                R[j] = arr[m + 1 + j];
+
+            i = 0;
+            j = 0;
+
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+    }
     // Quick Sort
     [Benchmark]
     public void QuickSort()
     {
-        // Your implementation of Quick Sort algorithm here
+        static void swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        static int partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];
+
+            int i = (low - 1);
+
+            for (int j = low; j <= high - 1; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    swap(arr, i, j);
+                }
+            }
+            swap(arr, i + 1, high);
+            return (i + 1);
+        }
+
+        static void quickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = partition(arr, low, high);
+
+                quickSort(arr, low, pi - 1);
+                quickSort(arr, pi + 1, high);
+            }
+        }
     }
 
     // Example usage
